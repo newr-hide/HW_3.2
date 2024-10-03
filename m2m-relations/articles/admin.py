533d20ae525ae353df647db48db3b04af587714b
Bondarev Artem <bond210484@mail.ru>
2024-10-03 15:32:39 +0700

    add 2 task

--- m2m-relations/articles/admin.py
+++ m2m-relations/articles/admin.py
@@ -1,8 +1,38 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Scope, Badge
 
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        for form in self.forms:
+
+            tmp_data = form.cleaned_data
+            if 'is_main' in tmp_data:  # проверка существования ключа
+                if tmp_data['is_main'] == True:
+                    count += 1
+            else:
+                continue
+        if count > 1:
+            raise ValidationError('Должен быть только один элемент с галочкой Основной')
+        elif count < 1:
+            raise ValidationError('Должен быть хотя бы один элемент с галочкой Основной')
+        else:
+            return super().clean()
+
+
+
+class RelationshipInline(admin.TabularInline):
+    model = Scope
+    formset = RelationshipInlineFormset
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [RelationshipInline]
+
+
+@admin.register(Badge)
+class BadgeAdmin(admin.ModelAdmin):
     pass
--- m2m-relations/articles/models.py
+++ m2m-relations/articles/models.py
@@ -1,5 +1,16 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
+
+class Badge(models.Model):
+    tags = models.CharField(max_length=255, blank=True)
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.tags
 
 class Article(models.Model):
 
@@ -7,10 +18,25 @@ class Article(models.Model):
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    # scopes = models.ManyToManyField(Badge)
 
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+
+class Scope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    scope = models.ForeignKey(Badge, on_delete=models.CASCADE, verbose_name='Тематика статьи')
+    is_main = models.BooleanField(verbose_name='Основной')
+
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = 'Разделы'
+
+    def __str__(self):
+        return self.scope.tags
+
